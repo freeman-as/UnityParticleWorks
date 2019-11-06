517a3a98a69f7adf5a8aebabdee76b07091792a8
Atsunori Sagawa <freeman.by2018@gmail.com>
2019-11-06 21:44:06 +0900

    [add] 18_BoxOpenShine scene.

---
+++ Assets/09_Dust/Scripts/Button_EffectPlay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_EffectPlay : MonoBehaviour {
+
+	private string nowScene;
+
+
+	void OnGUI () {
+ 		if(GUI.Button(new Rect(20,40,80,20), " Play")) {
+			nowScene = Application.loadedLevelName;
+			Application.LoadLevel(nowScene);
+		}
+	}
+}
